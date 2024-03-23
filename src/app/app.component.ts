aef888e60a6b2d803a0568981715a46c90cfa35f
Stephen Rinkus <jasperwednesday@gmail.com>
2024-03-22 18:14:25 -0700

    adding styles folder and using its styles.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';  
+// import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ang-optimized';
+
+  title1 = 'ang-optimized';
+  
+  constructor() {}
+    
+    // ngOnInit(): void {
+    //   if (1 > 3){
+    //     console.log("true");
+    //   }else{
+    //     console.log("false");
+    //   }
+    // }
+
+    title = 'scss-app'; 
+    showModal: boolean = false; 
+    openModal() { 
+        this.showModal = true; 
+    } 
+  
+    closeModal() { 
+        this.showModal = false; 
+    } 
+
 }
